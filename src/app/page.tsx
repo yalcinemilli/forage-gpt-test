2d040dd8edbea8c875bd7ded8b76fdc283cd7f81
Mark Maurer <hey@markmaurer.de>
2025-07-03 21:03:18 +0200

    full width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -268,7 +268,7 @@ export default function Home() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-6">  
+    <div className="w-full p-4 space-y-6">  
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <textarea
