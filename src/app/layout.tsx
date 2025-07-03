7b94877785104c0a4c1f98f66656dc45a324cb7c
Yalcin Emilli <97788204+yalcinemilli@users.noreply.github.com>
2025-07-03 17:58:15 +0200

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* ZAF SDK einbinden */}
+        <Script
+          src="https://static.zdassets.com/zendesk_app_framework_sdk/2.0/zaf_sdk.min.js"
+          strategy="beforeInteractive"
+        />
         {children}
       </body>
     </html>
